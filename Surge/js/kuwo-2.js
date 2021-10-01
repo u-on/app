5d714bc29680558b91b2c09447955d7c4c2a017c
u-on <26396078+u-on@users.noreply.github.com>
2021-10-02 00:45:44 +0800

    ~

--- Surge/js/kuwo-2.js
+++ Surge/js/kuwo-2.js
@@ -29,14 +29,14 @@ const vip = '/vip/v2/user/vip';
 const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "1";
-	obj.data["vipLuxuryExpire"] = "1654068149000";
-	obj.data["time"] = "1654068149000";
+	obj.data["isNewUser"] = "2";
+	obj.data["vipLuxuryExpire"] = "1835312949000";
+	obj.data["time"] = "1961170340993";
 	obj.data["isYearUser"] = "2";
-	obj.data["vipmExpire"] = "1654068149000";
+	obj.data["vipmExpire"] = "1835312949000";
 	obj.data["vipOverSeasExpire"] = "1835312949000";
-	obj.data["vipExpire"] = "1654068149000";
-	obj.data["vip3Expire"] = "1654068149000";
+	obj.data["vipExpire"] = "1835312949000";
+	obj.data["vip3Expire"] = "1835312949000";
 	body = JSON.stringify(obj);
 }
 
